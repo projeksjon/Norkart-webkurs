7162ce49451fd8029463ff1a3909316d7bd02a5f
Mathilde <mathilde.orstavik@gmail.com>
2017-02-19 21:14:40 +0100

    added basemap explenation

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # Norkart-webkurs
 
-##Innhold:
-[Basemap]
 
-##Bakgrunnskart (Basemap) [basemap]
+## Innhold:
 
-https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF0aGlsZG8iLCJhIjoiY2lrdHZvMHdsMDAxMHdvbTR0MWZkY3FtaCJ9.u4bFYLBtEGNv4Qaa8Uaqzw
+[Innhold:](#innhold)
+- [Bakgrunnskart](#bakgrunnskart)
+- [Popup](#popup)
 
 
+## Bakgrunnskart
+
+Bakgrunsskart definerer stilen på kartet, og kan enkelt byttes i koden i `public/js/index.js`.
+
+```javascript
+
+    var basemapUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+      L.tileLayer(basemapUrl).addTo(map);
+
+```
+Her er noen eksempler på bakgrunnskart dere kan bruke:
+
+[grey](./public/images/grey.png)
+[satellite](./public/images/satellite.png)
+[streets](./public/images/streets.png)
+[osm]('./public/images/osm.png')
+http://{s}.tile.osm.org/{z}/{x}/{y}.png
+
+https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF0aGlsZG8iLCJhIjoiY2lrdHZvMHdsMDAxMHdvbTR0MWZkY3FtaCJ9.u4bFYLBtEGNv4Qaa8Uaqzw
--- public/js/index.js
+++ public/js/index.js
@@ -6,14 +6,17 @@ function setMap() {
 
       var map = L.map('map').setView([63.430, 10.395], 14);
       //Set view takes two parameters;
-            //1. The cooridantes for the center of the map
+            //1. The coordinates for the center of the map
             //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
 
 
-      //Adding the base map --> how the background map looks like
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
+      //Adding the base map. Base map decides how the background map looks like
+      var basemapUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+      // var basemapUrl='https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF0aGlsZG8iLCJhIjoiY2lrdHZvMHdsMDAxMHdvbTR0MWZkY3FtaCJ9.u4bFYLBtEGNv4Qaa8Uaqzw'
+      var basemapUrl='https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF0aGlsZG8iLCJhIjoiY2lrdHZvMHdsMDAxMHdvbTR0MWZkY3FtaCJ9.u4bFYLBtEGNv4Qaa8Uaqzw'
+      var basemapUrl='https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF0aGlsZG8iLCJhIjoiY2lrdHZvMHdsMDAxMHdvbTR0MWZkY3FtaCJ9.u4bFYLBtEGNv4Qaa8Uaqzw'
+      L.tileLayer(basemapUrl).addTo(map);
+
 
       //Adding the carto layer to the map:
       cartodb.createLayer(map, 'https://mathildeo.carto.com/api/v2/viz/808e5efa-f6bd-11e6-bd76-0ee66e2c9693/viz.json', { https: true })
