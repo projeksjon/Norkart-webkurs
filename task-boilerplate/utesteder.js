d98ba6c05547615d1bd3befe249d596f0f662cf3
Anne Sofie Strand Erichsen <aserichs@stud.ntnu.no>
2017-03-14 19:20:02 +0100

    Update utesteder.js

--- task-boilerplate/utesteder.js
+++ task-boilerplate/utesteder.js
@@ -8,9 +8,6 @@ var utesteder = {
       "type": "Feature",
       "id": "node/33209415",
       "properties": {
-        "@id": "node/33209415",
-        "amenity": "pub",
-        "microbrewery": "yes",
         "name": "Daglighallen"
       },
       "geometry": {
@@ -25,16 +22,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/34926021",
       "properties": {
-        "@id": "node/34926021",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "66",
-        "addr:postcode": "7013",
-        "addr:street": "Øvre Bakklandet",
-        "alt_name": "Nabo'n",
-        "amenity": "pub",
         "name": "Den Gode Nabo",
-        "website": "www.dengodenabo.com",
-        "wheelchair": "no"
       },
       "geometry": {
         "type": "Point",
@@ -48,18 +36,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/153142045",
       "properties": {
-        "@id": "node/153142045",
-        "amenity": "pub",
-        "craft": "brewery",
-        "food": "yes",
-        "microbrewery": "yes",
         "name": "Trondhjem Mikrobryggeri",
-        "opening_hours": "Mo 15:00-24:00; Th-Fr 15:00-02:00; Sa 12:00-02:00; Su Closed",
-        "outdoor_seating": "yes",
-        "website": "http://www.tmb.no/",
-        "wheelchair": "limited",
-        "wikidata": "Q12007654",
-        "wikipedia": "no:Trondhjem Mikrobryggeri"
       },
       "geometry": {
         "type": "Point",
@@ -71,35 +48,8 @@ var utesteder = {
     },
     {
       "type": "Feature",
-      "id": "node/178228376",
-      "properties": {
-        "@id": "node/178228376",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "24A",
-        "addr:postcode": "7042",
-        "addr:street": "Strandveien",
-        "amenity": "pub",
-        "cuisine": "burger",
-        "food": "yes",
-        "name": "Ramp Pub & Spiseri",
-        "toilets:wheelchair": "no",
-        "website": "http://lamoramp.net/",
-        "wheelchair": "no"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.4215673,
-          63.4379862
-        ]
-      }
-    },
-    {
-      "type": "Feature",
       "id": "node/270521798",
       "properties": {
-        "@id": "node/270521798",
-        "amenity": "pub",
         "name": "Antikvariatet"
       },
       "geometry": {
@@ -114,10 +64,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/270521799",
       "properties": {
-        "@id": "node/270521799",
-        "amenity": "pub",
         "name": "Bare Blåbær",
-        "wheelchair": "yes"
       },
       "geometry": {
         "type": "Point",
@@ -131,10 +78,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/270521800",
       "properties": {
-        "@id": "node/270521800",
-        "amenity": "pub",
         "name": "Bær og Bar",
-        "wheelchair": "yes"
       },
       "geometry": {
         "type": "Point",
@@ -146,85 +90,9 @@ var utesteder = {
     },
     {
       "type": "Feature",
-      "id": "node/283156629",
-      "properties": {
-        "@id": "node/283156629",
-        "amenity": "pub",
-        "loc_name": "Norsk Aquavit Bar no. 1",
-        "name": "Erich Christians Skjenkestue"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3975187,
-          63.4336823
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/283156631",
-      "properties": {
-        "@id": "node/283156631",
-        "amenity": "pub",
-        "name": "Cafe 3B",
-        "wheelchair": "no"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.4015808,
-          63.4339028
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/365286717",
-      "properties": {
-        "@id": "node/365286717",
-        "amenity": "nightclub",
-        "name": "Me nightclub",
-        "opening_hours": "Fr-Sa 22:00-02:00",
-        "website": "http://me-nightclub.com/",
-        "wheelchair": "no"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3931328,
-          63.4318383
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/386579887",
-      "properties": {
-        "@id": "node/386579887",
-        "amenity": "bar",
-        "name": "Bari café & bar",
-        "opening_hours": "Mo 11:00-00:00;Tu-Th 11:00-01:00;Fr-Sa 11:00-02:00",
-        "phone": "73 60 60 24",
-        "website": "http://bari.no/",
-        "wheelchair": "no"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3945606,
-          63.432238
-        ]
-      }
-    },
-    {
-      "type": "Feature",
       "id": "node/386579903",
       "properties": {
-        "@id": "node/386579903",
-        "amenity": "bar",
         "name": "Hjørnet bar",
-        "wheelchair": "limited"
       },
       "geometry": {
         "type": "Point",
@@ -236,37 +104,9 @@ var utesteder = {
     },
     {
       "type": "Feature",
-      "id": "node/576557075",
-      "properties": {
-        "@id": "node/576557075",
-        "amenity": "pub",
-        "name": "Lucas",
-        "opening_hours": "Mo-Th 14:00-23:00; Fr-Sa 13:00-24:00; Su 13:00-23:00"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3995318,
-          63.4221396
-        ]
-      }
-    },
-    {
-      "type": "Feature",
       "id": "node/893397017",
       "properties": {
-        "@id": "node/893397017",
-        "addr:city": "Trondheim",
-        "addr:country": "NO",
-        "addr:street": "Holstveita",
-        "amenity": "pub",
         "name": "Kieglekroa",
-        "opening_hours": "Mo-We 15:00-00:00; Th 15:00-01:00; Sa 13:00-02:00; Fr 15:00-02:00",
-        "phone": "73 80 70 80",
-        "source": "survey",
-        "website": "http://www.kieglekroa.no/",
-        "wikidata": "Q18606625",
-        "wikipedia": "no:Kieglekroa"
       },
       "geometry": {
         "type": "Point",
@@ -280,8 +120,6 @@ var utesteder = {
       "type": "Feature",
       "id": "node/948503915",
       "properties": {
-        "@id": "node/948503915",
-        "amenity": "nightclub",
         "name": "Metro"
       },
       "geometry": {
@@ -296,17 +134,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/956343630",
       "properties": {
-        "@id": "node/956343630",
-        "addr:city": "Trondheim",
-        "addr:country": "NO",
-        "addr:housenumber": "27",
-        "addr:postcode": "7011",
-        "addr:street": "Olav Tryggvasons gate",
-        "amenity": "nightclub",
         "name": "Circus",
-        "phone": "93 46 11 00",
-        "website": "http://www.barcircus.no/",
-        "wheelchair": "limited"
       },
       "geometry": {
         "type": "Point",
@@ -320,17 +148,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/956343651",
       "properties": {
-        "@id": "node/956343651",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "11",
-        "addr:postcode": "7011",
-        "addr:street": "Dronningens gate",
-        "amenity": "pub",
         "name": "Cock & Balls",
-        "old_name": "Sentralkafeen",
-        "opening_hours": "Tu-Sa 21:00-02:30",
-        "website": "https://www.facebook.com/familientrondheim",
-        "wheelchair": "limited"
       },
       "geometry": {
         "type": "Point",
@@ -342,35 +160,9 @@ var utesteder = {
     },
     {
       "type": "Feature",
-      "id": "node/1984154033",
-      "properties": {
-        "@id": "node/1984154033",
-        "amenity": "pub",
-        "cuisine": "coffee_shop",
-        "name": "Mellomveien"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.4288073,
-          63.4381711
-        ]
-      }
-    },
-    {
-      "type": "Feature",
       "id": "node/2114218292",
       "properties": {
-        "@id": "node/2114218292",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "2",
-        "addr:postcode": "7011",
-        "addr:street": "Nedre Enkeltskillingsveita",
-        "amenity": "pub",
-        "food": "yes",
         "name": "Mormors Stue",
-        "outdoor_seating": "yes",
-        "website": "http://www.mormor.no/"
       },
       "geometry": {
         "type": "Point",
@@ -384,15 +176,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/2275943674",
       "properties": {
-        "@id": "node/2275943674",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "26",
-        "addr:postcode": "7010",
-        "addr:street": "Nordre gate",
-        "amenity": "nightclub",
         "name": "Studio 26",
-        "opening_hours": "Th-Sa 22:00-02:30",
-        "website": "http://www.studio26.no/"
       },
       "geometry": {
         "type": "Point",
@@ -406,11 +190,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/2275948724",
       "properties": {
-        "@id": "node/2275948724",
-        "amenity": "pub",
         "name": "Lille London",
-        "opening_hours": "Mo-Sa 11:00-02:00; Su 12:00-02:00",
-        "website": "http://wp.lillelondon.no/"
       },
       "geometry": {
         "type": "Point",
@@ -424,33 +204,13 @@ var utesteder = {
       "type": "Feature",
       "id": "node/2275950820",
       "properties": {
-        "@id": "node/2275950820",
-        "amenity": "bar",
         "name": "Kos Sushilounge",
-        "website": "http://www.kosbar.no/"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
           10.3984579,
           63.4339653
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/2275966431",
-      "properties": {
-        "@id": "node/2275966431",
-        "amenity": "bar",
-        "name": "Bobbys Bar",
-        "website": "http://www.35786.websys.sysedata.no/"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3995952,
-          63.4338089
         ]
       }
     },
@@ -472,57 +232,9 @@ var utesteder = {
     },
     {
       "type": "Feature",
-      "id": "node/2302969753",
-      "properties": {
-        "@id": "node/2302969753",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "5",
-        "addr:postcode": "7011",
-        "addr:street": "Olav Tryggvasons gate",
-        "amenity": "bar",
-        "name": "Moskus",
-        "opening_hours": "We-Sa 18:00-02:30",
-        "website": "http://barmoskus.no/"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.4009631,
-          63.4332185
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/2371362255",
-      "properties": {
-        "@id": "node/2371362255",
-        "amenity": "pub",
-        "name": "Cafe Dublin",
-        "opening_hours": "Mo-Th 15:30-24:00; Fr 15:30-02:00; Sa 13:00-02:00; Su 15:00-24:00",
-        "website": "http://www.cafedublin.no/"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3925933,
-          63.430405
-        ]
-      }
-    },
-    {
-      "type": "Feature",
       "id": "node/2930084190",
       "properties": {
-        "@id": "node/2930084190",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "3",
-        "addr:postcode": "7010",
-        "addr:street": "Carl Johans gate",
-        "amenity": "bar",
         "name": "Diskoteket",
-        "opening_hours": "Mo-Sa 20:00-02:30",
-        "smoking": "outside"
       },
       "geometry": {
         "type": "Point",
@@ -536,8 +248,6 @@ var utesteder = {
       "type": "Feature",
       "id": "node/2957446762",
       "properties": {
-        "@id": "node/2957446762",
-        "amenity": "bar",
         "name": "The Mint bar & nightclub"
       },
       "geometry": {
@@ -552,14 +262,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/2957769633",
       "properties": {
-        "@id": "node/2957769633",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "17",
-        "addr:postcode": "7042",
-        "addr:street": "Tmv-Kaia",
-        "amenity": "bar",
         "name": "BrukBar / Blæst",
-        "website": "http://www.brukbar.no/"
       },
       "geometry": {
         "type": "Point",
@@ -571,34 +274,9 @@ var utesteder = {
     },
     {
       "type": "Feature",
-      "id": "node/2967929300",
-      "properties": {
-        "@id": "node/2967929300",
-        "addr:housenumber": "22B",
-        "addr:street": "Søndregata",
-        "amenity": "pub",
-        "name": "Macbeth Scottish Pub",
-        "website": "http://www.macbeth.no"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3999564,
-          63.433982
-        ]
-      }
-    },
-    {
-      "type": "Feature",
       "id": "node/2968701935",
       "properties": {
-        "@id": "node/2968701935",
-        "amenity": "pub",
-        "contact:street": "Cicignons plass",
         "name": "Olavs Pub og Spiseri",
-        "opening_hours": "Jan-Dec: Mo-Th 15:00-23:00; Jan-Dec: Fr 15:00-02:00; Jan-Dec: Sa 12:00-02:00",
-        "sustenance": "pub",
-        "website": "http://www.nova-trondheim.no/olavs-pub"
       },
       "geometry": {
         "type": "Point",
@@ -612,14 +290,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/2968721687",
       "properties": {
-        "@id": "node/2968721687",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "10-12B",
-        "addr:postcode": "7010",
-        "addr:street": "Brattørgata",
-        "amenity": "pub",
         "name": "Three Lions English pub",
-        "website": "http://www.threelions.no/"
       },
       "geometry": {
         "type": "Point",
@@ -633,10 +304,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/2969467028",
       "properties": {
-        "@id": "node/2969467028",
-        "amenity": "pub",
         "name": "Fru Lundgreen",
-        "website": "http://mirk.no/lundis/"
       },
       "geometry": {
         "type": "Point",
@@ -650,12 +318,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/3052351994",
       "properties": {
-        "@id": "node/3052351994",
-        "amenity": "nightclub",
         "name": "Dreams Showbar Trondheim",
-        "opening_hours": "Mo-Sa 22:00-04:00",
-        "operator": "Dreams Showbar",
-        "website": "http://www.dreams.no/trondheim"
       },
       "geometry": {
         "type": "Point",
@@ -669,10 +332,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/3056289080",
       "properties": {
-        "@id": "node/3056289080",
-        "amenity": "nightclub",
         "name": "Wildside",
-        "website": "http://thewildside.no/"
       },
       "geometry": {
         "type": "Point",
@@ -684,70 +344,9 @@ var utesteder = {
     },
     {
       "type": "Feature",
-      "id": "node/3060093856",
-      "properties": {
-        "@id": "node/3060093856",
-        "amenity": "nightclub",
-        "name": "Red Zone",
-        "website": "https://www.facebook.com/pages/ACE-Nattklubb/141563669287388"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3964446,
-          63.4346296
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/3060093863",
-      "properties": {
-        "@id": "node/3060093863",
-        "amenity": "pub",
-        "name": "Kafé Larssen",
-        "website": "http://www.kafelarssen.no/"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3963643,
-          63.4345962
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/3409770733",
-      "properties": {
-        "@id": "node/3409770733",
-        "alt_name": "Daniel på hjørnet",
-        "amenity": "pub",
-        "name": "Daniel"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.4025517,
-          63.4319605
-        ]
-      }
-    },
-    {
-      "type": "Feature",
       "id": "node/3581237893",
       "properties": {
-        "@id": "node/3581237893",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "20D",
-        "addr:postcode": "7014",
-        "addr:street": "Nedre Bakklandet",
-        "amenity": "pub",
-        "microbrewery": "yes",
         "name": "Taps",
-        "opening_hours": "Tu-Th 17:00-00:00; Fr-Sa 17:00-01:30",
-        "operator": "Monkey Brew",
-        "website": "https://www.facebook.com/TapsTrondheim"
       },
       "geometry": {
         "type": "Point",
@@ -761,10 +360,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/3834083060",
       "properties": {
-        "@id": "node/3834083060",
-        "amenity": "nightclub",
         "name": "Fire Fine",
-        "website": "https://www.facebook.com/firefinesolsiden"
       },
       "geometry": {
         "type": "Point",
@@ -778,12 +374,7 @@ var utesteder = {
       "type": "Feature",
       "id": "node/3985714094",
       "properties": {
-        "@id": "node/3985714094",
-        "amenity": "bar",
         "name": "Work-Work",
-        "opening_hours": "Mo-Fr 12:00-00:00; Sa-Su 12:00+",
-        "pub": "gaming_bar",
-        "website": "http://work-work.no/gaming/"
       },
       "geometry": {
         "type": "Point",
@@ -797,8 +388,6 @@ var utesteder = {
       "type": "Feature",
       "id": "node/4369444243",
       "properties": {
-        "@id": "node/4369444243",
-        "amenity": "nightclub",
         "name": "Blæst"
       },
       "geometry": {
@@ -813,8 +402,6 @@ var utesteder = {
       "type": "Feature",
       "id": "node/4412824636",
       "properties": {
-        "@id": "node/4412824636",
-        "amenity": "nightclub",
         "name": "Jesper Nattmann"
       },
       "geometry": {
@@ -827,31 +414,8 @@ var utesteder = {
     },
     {
       "type": "Feature",
-      "id": "node/4414434160",
-      "properties": {
-        "@id": "node/4414434160",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "30",
-        "addr:postcode": "7011",
-        "addr:street": "Olav Tryggvasons gate",
-        "amenity": "pub",
-        "name": "BEAR Brew & Pizza",
-        "wheelchair": "yes"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3954109,
-          63.4333699
-        ]
-      }
-    },
-    {
-      "type": "Feature",
       "id": "node/4427750714",
       "properties": {
-        "@id": "node/4427750714",
-        "amenity": "bar",
         "name": "Manhattan"
       },
       "geometry": {
@@ -859,43 +423,6 @@ var utesteder = {
         "coordinates": [
           10.3986347,
           63.4320372
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/4427750716",
-      "properties": {
-        "@id": "node/4427750716",
-        "amenity": "bar",
-        "name": "Red Piano"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.3986568,
-          63.4320702
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "id": "node/4630232863",
-      "properties": {
-        "@id": "node/4630232863",
-        "addr:city": "Trondheim",
-        "addr:housenumber": "50",
-        "addr:postcode": "7068",
-        "addr:street": "Innherredsveien",
-        "amenity": "pub",
-        "name": "Ladejarlen Gastropub",
-        "smoking": "no"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          10.4202736,
-          63.4362199
         ]
       }
     }
