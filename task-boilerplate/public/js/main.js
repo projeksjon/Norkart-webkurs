373cf7bb971311e9ef6b1511836ed82c3ee3c1f0
Mathilde <mathilde.orstavik@gmail.com>
2017-11-23 13:12:22 +0100

    updated for uia course

--- task-boilerplate/public/js/main.js
+++ task-boilerplate/public/js/main.js
@@ -4,10 +4,10 @@ function setMap() {
 
       console.log('Loading map');
 
-      var map = L.map('map').setView([63.430, 10.395], 14);
       //Set view takes two parameters;
             //1. The coordinates for the center of the map
             //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
+      var map = L.map('map').setView([58.15381, 7.99641], 13.5);
 
 
       //Adding the base map. Base map decides how the background map looks like
