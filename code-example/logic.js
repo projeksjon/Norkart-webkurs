2c9114241e9cb744bb3dd8186594a1462834c259
Anne Sofie Strand Erichsen <anneri@norkart.no>
2018-01-23 19:15:28 +0100

    revert the state of the project

---
+++ code-example/logic.js
@@ -0,0 +1,8 @@
+
+function showImage(id) {
+	document.getElementById(id).className = 'show';
+}
+
+function hideButton(id){
+	document.getElementById(id).className="hide";
+}
---
+++ task-boilerplate/utesteder.js
@@ -0,0 +1,372 @@
+var utesteder = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2017-02-08T08:54:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "id": "node/33209415",
+      "properties": {
+        "name": "Studenter Samfundet"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3957807,
+          63.4224338
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/34926021",
+      "properties": {
+        "name": "Den Gode Nabo",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4023582,
+          63.4279411
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/153142045",
+      "properties": {
+        "name": "Trondhjem Mikrobryggeri",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3928913,
+          63.4313285
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/270521798",
+      "properties": {
+        "name": "Antikvariatet"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4027996,
+          63.4284213
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/270521799",
+      "properties": {
+        "name": "Bare Blåbær",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4122761,
+          63.4338923
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/270521800",
+      "properties": {
+        "name": "Bær og Bar",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4119105,
+          63.4337556
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/893397017",
+      "properties": {
+        "name": "Kieglekroa",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3907764,
+          63.431219
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/948503915",
+      "properties": {
+        "name": "Metro"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4005447,
+          63.4292233
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/956343630",
+      "properties": {
+        "name": "Circus",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3975857,
+          63.4330878
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/956343651",
+      "properties": {
+        "name": "Cock & Balls",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3934188,
+          63.4318452
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2114218292",
+      "properties": {
+        "name": "Mormors Stue",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.395094,
+          63.4327331
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2275943674",
+      "properties": {
+        "name": "Studio 26",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3969156,
+          63.4340221
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2275948724",
+      "properties": {
+        "name": "Lille London",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3982194,
+          63.4339675
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2275950820",
+      "properties": {
+        "name": "Kos Sushilounge",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3984579,
+          63.4339653
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2930084190",
+      "properties": {
+        "name": "Diskoteket",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3993036,
+          63.4338164
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2957446762",
+      "properties": {
+        "name": "The Mint bar & nightclub"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3974454,
+          63.4342639
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2957769633",
+      "properties": {
+        "name": "BrukBar / Blæst",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4118575,
+          63.4346315
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2968701935",
+      "properties": {
+        "name": "Olavs Pub og Spiseri",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4014134,
+          63.4327413
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2968721687",
+      "properties": {
+        "name": "Three Lions English pub",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4012364,
+          63.4341322
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3052351994",
+      "properties": {
+        "name": "Dreams Showbar Trondheim",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3915971,
+          63.4336681
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3056289080",
+      "properties": {
+        "name": "Wildside",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3974573,
+          63.4343806
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3581237893",
+      "properties": {
+        "name": "Taps",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.403777,
+          63.4294777
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3834083060",
+      "properties": {
+        "name": "Fire Fine",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4071069,
+          63.432825
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3985714094",
+      "properties": {
+        "name": "Work-Work",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3933047,
+          63.4328722
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4369444243",
+      "properties": {
+        "name": "Blæst"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4119109,
+          63.434667
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4427750714",
+      "properties": {
+        "name": "Manhattan"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.3986347,
+          63.4320372
+        ]
+      }
+    }
+  ]
+};
