e19e6f3985401b6fe8595b8da550e6af27ae3cd7
Anne Sofie Strand Erichsen <anneri@norkart.no>
2019-02-06 13:29:25 +0100

    add comments

--- task-boilerplate/public/js/main.js
+++ task-boilerplate/public/js/main.js
@@ -1,22 +1,18 @@
-
 //Initializing the map
 function setMap() {
+  console.log("Loading map");
 
-      console.log('Loading map');
+  var map = L.map("map").setView([63.43, 10.395], 14);
+  //Set view takes two parameters;
+  //1. The coordinates for the center of the map
+  //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
 
-      var map = L.map('map').setView([63.430, 10.395], 14);
-      //Set view takes two parameters;
-            //1. The coordinates for the center of the map
-            //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
+  //Adding the base map. Base map decides how the background map looks like
+  var basemapUrl = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
+  L.tileLayer(basemapUrl).addTo(map);
 
-
-      //Adding the base map. Base map decides how the background map looks like
-      var basemapUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
-      L.tileLayer(basemapUrl).addTo(map);
-
-      //Adding geoJSON layer to the map:
-      L.geoJSON(utesteder)
-      .addTo(map);
+  //Adding geoJSON layer to the map:
+  L.geoJSON(utesteder).addTo(map);
 }
 
 window.onload = setMap;
