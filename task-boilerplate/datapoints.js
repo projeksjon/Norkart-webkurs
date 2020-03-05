cd0bdc68ce9a69e016e55c2a71efd7bff8b1a926
Mathilde Ørstavik <mators@norkart.no>
2020-03-05 12:15:15 +0100

    added new task, gløs guide

---
+++ task-boilerplate/datapoints.js
@@ -0,0 +1,14 @@
+var datapoints = {
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "Geomatikk lesesalen"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [10.3957807, 63.4224338]
+      }
+    }
+  ]
+};
--- task-boilerplate/public/js/main.js
+++ task-boilerplate/public/js/main.js
@@ -2,7 +2,7 @@
 function setMap() {
   console.log("Loading map");
 
-  var map = L.map("map").setView([63.43, 10.395], 14);
+  var map = L.map("map").setView([63.41761015625138, 10.404267311096193], 16);
   //Set view takes two parameters;
   //1. The coordinates for the center of the map
   //2. The zoom level. Zoomlevel is from 0 -> 22, where 22 is zoomed in an 0 is zoomed out
@@ -11,8 +11,24 @@ function setMap() {
   var basemapUrl = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
   L.tileLayer(basemapUrl).addTo(map);
 
-  //Adding geoJSON layer to the map:
-  L.geoJSON(utesteder).addTo(map);
+  //Creating marker in map
+  var lesesal_geomatikk_marker = L.marker([
+    63.414789466893424,
+    10.406202357262375
+  ]);
+
+  lesesal_geomatikk_marker.addTo(map);
+  lesesal_geomatikk_marker.on("click", function(e) {
+    console.log("clicked on point");
+    console.log(e.latlng);
+  });
+
+  map.on("click", handleMapClick);
+}
+
+function handleMapClick(event) {
+  console.log("map was clicked");
+  console.log(event);
 }
 
 window.onload = setMap;
